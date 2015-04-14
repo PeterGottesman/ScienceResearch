13c47a9b4bf5e8a55eeca4c60ac014e68f8e08c2
PeterGottesman <mygames1030@gmail.com>
2015-04-13 22:40:42 -0400

    I really need to install mpi on my computer but I don't have time to do it right now, so instead I am uploading to git and then downloading it to my cluster. This is nearly as ineffiecent as this commit message is long.

--- BigArrays/bigarrays.c
+++ BigArrays/bigarrays.c
@@ -21,7 +21,7 @@ int main (int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     arraysize = rank*size;
-    nums = (int*) malloc(sizeof(int)*arraysize)
+    nums = (int*) malloc(sizeof(int)*arraysize);
  
     MPI_Comm_size(MPI_COMM_WORLD, &nprocesses);
  
