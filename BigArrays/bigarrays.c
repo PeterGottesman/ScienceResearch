8f0aa6f98717eaea6cb1d3db00e765d85977f830
PeterGottesman <mygames1030@gmail.com>
2015-04-26 15:12:18 -0400

    unworking old version

--- BigArrays/bigarrays.c
+++ BigArrays/bigarrays.c
@@ -10,7 +10,7 @@ int main (int argc, char* argv[])
     int correct, rcorrect;
     int *nums, *Rnums, *test;
     MPI_Status status;
-    MPI_Request request;
+    MPI_Request request, request2;
 
     root = 0;
     size = 50000;
@@ -33,9 +33,9 @@ int main (int argc, char* argv[])
             for (iterator = 0; iterator < arraysize; iterator++) {
                 nums[iterator] = (rank + target) * (iterator + 1); 
             }
-            printf("w1 %d \n", rank);
-            MPI_Send(nums, arraysize, MPI_INT, target, tag, MPI_COMM_WORLD);
-            printf("ie1 %d \n", rank);
+            printf("presend %d to %d\n", rank, target);
+            MPI_Isend(nums, arraysize, MPI_INT, target, tag, MPI_COMM_WORLD, &request2);
+            printf("sent %d to %d\n", rank, target);
         }
     } 
  
@@ -47,21 +47,21 @@ int main (int argc, char* argv[])
                 test[iterator] = (source + rank) * (iterator + 1);
             }
             Rnums = (int*) malloc(sizeof(int)*recvsize);
+            MPI_Wait(&request2, &status);
             MPI_Irecv(Rnums, recvsize, MPI_INT, source, tag, MPI_COMM_WORLD, &request);
-            printf("1 %d \n", rank);
+            printf("wait %d from %d\n", rank, source);
             MPI_Wait(&request, &status);
-            printf("%d \n", rank);
-            if (rank != root) {
-                printf("I guess the problem is here? \n");
+            //if (rank != root) {
+                printf("%d checking send of %d\n", rank, source);
                 correct =  memcmp(Rnums, test, recvsize);
                 if (correct != 0) {
-                    printf("rank %d got incorrect data\n", rank);
-                    MPI_Abort(MPI_COMM_WORLD, 1);
+                    printf("-------------rank %d got incorrect data from %d\n", rank, source);
+                    //MPI_Abort(MPI_COMM_WORLD, 1);
                 } else {
                     printf("rank %d got correct data from %d \n", rank, source);
                 }
                 //MPI_Send(&correct, 1, MPI_INT, root, tag, MPI_COMM_WORLD);
-            }
+            //}
             free(Rnums);
             free(test);
         }
@@ -69,6 +69,7 @@ int main (int argc, char* argv[])
             //MPI_Recv(&rcorrect, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
         }
     }
+
 
     free(nums);
 
