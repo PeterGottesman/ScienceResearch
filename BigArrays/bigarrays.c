00277211ca6a1056b8e8cea166416c1065e7fe9b
PeterGottesman <mygames1030@gmail.com>
2015-04-29 15:04:07 -0400

    pre-meeting V.2

--- BigArrays/bigarrays.c
+++ BigArrays/bigarrays.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "mpi.h"
- 
+
 int main (int argc, char* argv[]) {
     int rank, root, nproc, tag, size, arraysize, recsize;
     int *nums, *rnums, *rectest;
@@ -12,34 +12,36 @@ int main (int argc, char* argv[]) {
     root = 0;
 
     MPI_Init(&argc, &argv);
- 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    arraysize = size*rank;
-    nums = (int *) malloc(sizeof(int)*arraysize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
     tag = 1;
+    MPI_Request request[nproc*2];
 
-    for (int sender=0; sender < nproc; sender++) {
-        if(rank==sender) {
+    for (int sender = 0; sender < nproc; sender++) {
+        arraysize += size * sender;
+    }
+    nums = (int *) malloc(sizeof(int) * arraysize);
+
+    int place = 0;
+    for (int sender = 0; sender < nproc; sender++) {
+        place += size * sender;
+        if(sender == rank) {
             for (int receiver = 0; receiver < nproc; receiver++) {
                 if(receiver!=rank) {
-                    for (int filler = 0; filler < arraysize; filler++) {
-                        nums[filler] = (rank + receiver) * (filler + 1);
-                    }
-                    MPI_Send(nums, arraysize, MPI_INT, receiver, tag, MPI_COMM_WORLD);
+                    MPI_Isend(nums[place], sender * size, MPI_INT, receiver, tag, MPI_COMM_WORLD, &request[sender+(nproc-1)]);
                 }
             }
         } else {
             recsize = size*sender;
-            rectest = (int *) malloc(sizeof(int)*recsize);
+            rectest = (int *) malloc(sizeof(int)*recsize)
             for (int filler = 0; filler < recsize; filler++) {
                 rectest[filler] = (rank + sender) * (filler + 1);
             }
             rnums = (int *) malloc(sizeof(int)*recsize);
-            MPI_Recv(rnums, recsize, MPI_INT, sender, tag, MPI_COMM_WORLD, &status);
+            MPI_Irecv(&rnums[place], size * sender, MPI_INT, tag, sender, MPI_COMM_WORLD, &request[sender]);
             if(memcmp(rectest, rnums, recsize)==0) {
                 printf("rank %d got correct data from %d \n", rank, sender);
             } else {
