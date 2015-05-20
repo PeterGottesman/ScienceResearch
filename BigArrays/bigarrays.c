a10c78478643616641899ca6cce9f5a539edd06a
PeterGottesman <mygames1030@gmail.com>
2015-05-19 21:07:05 -0400

    Added automation and fixed incorrect data analysis Data Analysis: --- When checking the data for correctness, the place was being incremented in a different point in the loop than the sends and receives. Automation: --- Added run.sh which will compile and run bigarrays with the specified number of processes. Additionally it will grep for search terms provided.
    *This does not currently support multi-word search terms*

--- BigArrays/bigarrays.c
+++ BigArrays/bigarrays.c
@@ -6,7 +6,7 @@
  
 int main (int argc, char* argv[]) {
     int rank, root, nproc, tag, size, arraysize;
-    int *nums;
+    int *nums, *rnums;
     bool correct;
 
     size = 50000;
@@ -26,43 +26,52 @@ int main (int argc, char* argv[]) {
     for (int sender=0; sender <= nproc; sender++) {
         arraysize+=size*sender;
     }
-    nums = (int *) malloc(sizeof(int)*arraysize);
+    rnums = (int *) malloc(sizeof(int)*arraysize);
+    if (rnums==NULL) {
+        fprintf(stderr, "rnums did not malloc correctly in %d\n", rank);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    nums = (int *) malloc(sizeof(int)*size*rank*(nproc-1));
+    if (nums==NULL) {
+        fprintf(stderr, "nums did not malloc correctly in %d\n", rank);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
 
     int place = 0;
     int numrequest = 0;
-    for (int sender=0; sender < nproc; sender++) {
-        place += size*sender;
-        if(rank==sender) {
-            for (int receiver = 0; receiver < nproc; receiver++) {
-                if(receiver!=rank) {
-                    for (int filler = 0; filler < size*sender; filler++) {
-                        nums[filler+place] = (rank + receiver) * (filler + 1);
-                    }
-                    printf("%d\n", nums[place+1]);
-                    MPI_Isend(&nums[place], size*sender, MPI_INT, receiver, tag, MPI_COMM_WORLD, &request[numrequest++]);
-                }
+    printf("sender %d is filling starting with position %d\n", rank, place);
+    for (int receiver = 0; receiver < nproc; receiver++) {
+        if(receiver!=rank) {
+            printf("sender %d is sending to receiver %d starting at position %d\n", rank, receiver, place);
+            for (int filler = 0; filler < size*rank; filler++) {
+                nums[filler+place] = (rank + receiver) * (filler + 1);
+                //nums[filler+place]=rank;
             }
+            printf("%d sent %d to %d\n", rank, nums[place], receiver);
+            MPI_Isend(&nums[place], size*rank, MPI_INT, receiver, tag, MPI_COMM_WORLD, &request[numrequest++]);
+            place += size*rank;
         }
     }
     
     place = 0;
     for (int sender = 0; sender < nproc; sender++) {
-        place += size*sender;
         if (rank!=sender) {
-            MPI_Irecv(&nums[place], size*sender, MPI_INT, sender, tag, MPI_COMM_WORLD, &request[numrequest++]);
+            MPI_Irecv(&rnums[place], size*sender, MPI_INT, sender, tag, MPI_COMM_WORLD, &request[numrequest++]);
+            place += size*sender;
         }
     }
     
     MPI_Waitall((nproc-1)*2, request, status);
+    free(nums);
     
     place = 0;
     for (int sender = 0; sender < nproc; sender++) {
-        place += size*sender;
         if (rank!=sender) {
+            printf("%d got %d from %d\n", rank, rnums[place], sender);
             correct = true;
             for (int filler = 0; filler < size*sender; filler++) {
-                if (nums[filler+place] != (rank + sender) * (filler + 1)) {
-                    printf("%d got incorrect data from %d\n", rank, sender);
+                if (rnums[filler+place] != (rank + sender) * (filler + 1)) {
+                    printf("%d got incorrect data from %d, expecting %d got %d at %d, or %d including place\n", rank, sender, (rank+sender)*(filler+1), rnums[filler+place], filler, filler+place);
                     correct = false;
                     break;
                 } 
@@ -70,10 +79,11 @@ int main (int argc, char* argv[]) {
             if (correct) {
                 printf("%d got correct data from %d\n", rank, sender);
             }
+            place += size*sender;
         }
     }
 
-    free(nums);
+    free(rnums);
 
     MPI_Finalize();
 
