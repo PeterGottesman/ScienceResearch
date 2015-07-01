07c02fb44f8addb8546bff127471c5d3ed529138
PeterGottesman <mygames1030@gmail.com>
2015-07-01 15:34:24 -0400

    Added Overload-Allowed, and added error checking in shell calls

--- BigArrays/run.py
+++ BigArrays/run.py
@@ -3,12 +3,13 @@
 #from pushover import Pushover
 import argparse
 from datetime import datetime
-from subprocess import call
+from subprocess import check_call, CalledProcessError
 from sys import argv
 
 def run(args):
     maxprocs = args.maxprocs
     verify = args.verify
+    overload = args.overload_allowed
     message_size = args.message_size
     binding = args.binding
     plot = args.plot
@@ -16,9 +17,7 @@ def run(args):
     if verify: vstring = "--verify "
     else: vstring = ""
 
-    if binding == 0: bind = "none"
-    elif binding == 1: bind = "core"
-    elif binding == 2: bind = "socket"
+    if overload: binding += ":overload-allowed" 
 
     if plot: pstring = "--plot "
     else: pstring = ""
@@ -26,16 +25,41 @@ def run(args):
     memory = ['sm', 'vader']
     time = datetime.now().strftime("%Y-%m-%d.%H%M%S")
 
-    call("mpicc -g -o bigarrays bigarrays.c -std=c11", shell=True)
-    call("mkdir -p data/" + time, shell=True)
+    try: 
+        check_call("mpicc -g -o bigarrays bigarrays.c -std=c11", shell=True)
+    except CalledProcessError as err:
+        print("Error in Compiling with code" + str(err.returncode))
+        exit(1)
+
+    try:
+        check_call("mkdir -p data/" + time, shell=True)
+    except CalledProcessError as err:
+        print("Error in creating data directory with error code" + str(err.returncode))
+        exit(1)
+
 
     for nprocs in range(2, maxprocs+1):
         for mem in memory:
-            filename = "data/" + time + "/np="+ str(nprocs) + "_shmem=" + mem + "log.out" 
-            call("mpirun -np  " + str(nprocs) + " --bind-to " + bind + " --mca btl " + mem + ",self bigarrays " + pstring + vstring + "--message_size " + str(message_size) + " > " + filename, shell=True)
+            base = "data/" + time + "/np="+ str(nprocs) + "_shmem=" + mem + "log"
+            logfile = base + ".out"
+            mpirun_logfile = base + ".mpirun"
+
+            run = "mpirun -np  " + str(nprocs) + " --bind-to " + binding + " --mca btl " + mem + ",self bigarrays " + pstring + vstring + "--message_size " + str(message_size) + " > " + logfile
+            
+            print(run)
+            f = open(mpirun_logfile, 'w')
+            f.write(run + "\n")
+            f.close()
+
+            try:
+                check_call(run, shell=True)
+            except CalledProcessError as err:
+                print("mpirun failed with code" + str(err.returncode))
+                exit(1)
+
 
             if plot:
-                f = open(filename, 'r')
+                f = open(logfile, 'r')
                 data = []
                 for line in f:
                     data += line.split()
@@ -51,15 +75,20 @@ def run(args):
                 f.close()
 
     if plot:
-        call("gnuplot -e \"sm='data/"+time+"/sm-data.txt'\" -e \"vader='data/"+time+"/vader-data.txt'\" -e \"out='data/"+time+"/output-graph.pdf'\" plot.gnu", shell=True)
+        try:
+            check_call("gnuplot -e \"sm='data/"+time+"/sm-data.txt'\" -e \"vader='data/"+time+"/vader-data.txt'\" -e \"out='data/"+time+"/output-graph.pdf'\" plot.gnu", shell=True)
+        except CalledProcessError as err:
+            print("plot failed with error code " + str(err.returncode))
+            exit(1)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("maxprocs", type=int, help="maximum number of processes to be run")
     parser.add_argument("--verify", action="store_true", help="verify the correctness of the data sent")
+    parser.add_argument("--overload-allowed", action="store_true", help="allow overload of cores")
     parser.add_argument("--message_size", default=50000, type=int, help="specify the size of each message sent, defaults to 50000")
-    parser.add_argument("--binding", type=int, default=0, help="set the bind type for mpi runtime. 0=bind-to-none(default) 1=bind-to-core 2=bind-to-socket")
+    parser.add_argument("--binding", help="set the bind type for mpi runtime. Should be none, core or socket")
     parser.add_argument("--plot", action="store_true", help="use gnuplot to plot the output data to pdf. Specifying this disables debug output")
     args = parser.parse_args()
     run(args)
